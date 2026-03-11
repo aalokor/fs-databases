5c67116041b426d7cb7840150b61bff6069f7392
aalokor <aalo.korkeaaho@gmail.com>
2026-03-11 20:51:49 +0200

    remove comments

--- models/index.js
+++ models/index.js
@@ -5,9 +5,6 @@ const User = require("./user");
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
-//User.sync({ alter: true });
-//Blog.sync({ alter: true });
-
 const syncAll = async () => {
   await sequelize.sync({ alter: true });
 };
