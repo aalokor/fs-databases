bb0af60dd87753fc0728f6ade677ae920fb214a2
aalokor <aalo.korkeaaho@gmail.com>
2026-03-11 20:50:17 +0200

    change worflow back again and change syncing

--- models/index.js
+++ models/index.js
@@ -1,11 +1,18 @@
+const { sequelize } = require("../util/db");
 const Blog = require("./blog");
 const User = require("./user");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
-Blog.sync({ alter: true });
-User.sync({ alter: true });
+//User.sync({ alter: true });
+//Blog.sync({ alter: true });
+
+const syncAll = async () => {
+  await sequelize.sync({ alter: true });
+};
+
+syncAll();
 
 module.exports = {
   Blog,
