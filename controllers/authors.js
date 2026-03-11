3a37005de6f3777ee23f90cd3e351442a36f482b
aalokor <aalo.korkeaaho@gmail.com>
2026-03-11 20:20:26 +0200

    add tests and fix api/authors. Add workflows

--- controllers/authors.js
+++ controllers/authors.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
   const authors = await Blog.findAll({
     attributes: [
       "author",
-      [Sequelize.fn("COUNT", Sequelize.col("*")), "articles"],
+      [Sequelize.fn("COUNT", Sequelize.col("*")), "blogs"],
       [Sequelize.fn("SUM", Sequelize.col("likes")), "likes"],
     ],
     group: ["author"],
