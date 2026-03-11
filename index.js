40c97302e9ab9f0c958f47a116d9f1b89f512af0
aalokor <aalo.korkeaaho@gmail.com>
2026-03-11 19:54:35 +0200

    add api/authors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { connectToDatabase } = require("./util/db");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const authorRouter = require("./controllers/authors");
 
 const { unknownEndpoint, errorHandler } = require("./util/middleware");
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/authors", authorRouter);
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
