5674379072d66347d71803e81deb630de1468e7e
aalokor <aalo.korkeaaho@gmail.com>
2026-03-13 14:55:17 +0200

    add initial migration

--- models/index.js
+++ models/index.js
@@ -1,15 +1,8 @@
-const { sequelize } = require("../util/db");
 const Blog = require("./blog");
 const User = require("./user");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
-
-const syncAll = async () => {
-  await sequelize.sync({ alter: true });
-};
-
-syncAll();
 
 module.exports = {
   Blog,
