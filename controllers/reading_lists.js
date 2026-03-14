4189cb727ede4bb6c69f785c683c16fc5d5d69af
aalokor <aalo.korkeaaho@gmail.com>
2026-03-14 20:44:10 +0200

    run more tests and fix reading list issues

--- controllers/reading_lists.js
+++ controllers/reading_lists.js
@@ -3,41 +3,46 @@ const { tokenExtractor } = require("../util/middleware");
 
 const { Blog, User, ReadingList } = require("../models");
 
-router.post("/", tokenExtractor, async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
     const { userId, blogId } = req.body;
-    const user = await User.findByPk(req.decodedToken.id);
 
-    if (userId !== user.id) {
-      return res.status(403).json({
-        error: "Error: User can only add blogs to their own list",
-      });
+    if (!userId || !blogId) {
+      return res.status(400).json({ error: "userId and blogId required" });
+    }
+
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ error: "Error: User does not exist" });
     }
 
     const blog = await Blog.findByPk(blogId);
     if (!blog) {
-      return res.status(404).json({
-        error: "Error: Blog does not exist",
-      });
+      return res.status(404).json({ error: "Error: Blog does not exist" });
     }
 
     const existing = await ReadingList.findOne({
-      where: { user_id: userId, blog_id: blogId },
+      where: { userId, blogId }, // use camelCase
     });
 
     if (existing) {
-      return res.status(400).json({
-        error: "Error: Blog is already in your reading list",
-      });
+      return res
+        .status(400)
+        .json({ error: "Error: Blog is already in your reading list" });
     }
 
     const newEntry = await ReadingList.create({
-      userId: userId,
-      blogId: blogId,
+      userId,
+      blogId,
       read: false,
     });
 
-    return res.json(newEntry);
+    return res.status(201).json({
+      id: newEntry.id,
+      user_id: newEntry.userId,
+      blog_id: newEntry.blogId,
+      read: newEntry.read,
+    });
   } catch (error) {
     next(error);
   }
@@ -52,7 +57,7 @@ router.put("/:id", tokenExtractor, async (req, res, next) => {
     }
 
     if (reading.userId !== req.decodedToken.id) {
-      return res.status(403).json({
+      return res.status(401).json({
         error:
           "Error: User can only change the status of entries in their own reading list",
       });
